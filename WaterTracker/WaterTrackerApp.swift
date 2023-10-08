d32051bf7ddf84eb189768700b6b879aad187351
Khawlah Khalid <khawlah1920@hotmail.com>
2023-10-08 11:21:18 +0300

    Initial Commit

---
+++ WaterTracker/WaterTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterTrackerApp.swift
+//  WaterTracker
+//
+//  Created by Khawlah Khalid on 08/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct WaterTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
